3954ce1e076547ffe974c8803e037c49b4b6727b
Fahim Muntasir <fahimmuntasir115@gmail.com>
2023-12-20 18:33:57 +0600

    Done Assignment

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const expenseList = document.getElementById("expense-list");
 const totalIncome = document.getElementById("total-income");
 const totalExpense = document.getElementById("total-expense");
 const totalBudget = document.getElementById("total-budget");
-const addDeleteButton = document.getElementById("add-delete-button");
 
 function formatMoney(value) {
   return Math.abs(Number(value)).toLocaleString(undefined, {
@@ -28,11 +27,23 @@ function calculateIncome() {
   }
   totalIncome.innerHTML = formatMoney(sum);
 }
+
+let sum = 0;
+  if (incomeList.children.length === 0) {
+    totalIncome.innerHTML = formatMoney(0);
+  } else {
+    for (let item of incomeList.children) {
+      const valueString = item.children[0].children[1].children[0].innerHTML.replace(/,/g, "");
+      sum += parseFloat(valueString);
+    }
+    totalIncome.innerHTML = formatMoney(sum);
+  }
 calculateIncome();
 
 /**
  * Task 1: Calculate total expense
  */
+
 function calculateExpense() {
   let sum = 0;
   for (let item of expenseList.children) {
@@ -51,9 +62,13 @@ calculateExpense();
  */
 
 function calculateBudget() {
-    let Income = parseFloat(totalIncome.innerHTML.replace(/,/g, ""));
-    let Expense = parseFloat(totalExpense.innerHTML.replace(/,/g, ""));
+    //let Income , Expense, Budget;
+    let Income = parseFloat(totalIncome.innerHTML.replace(/,/g, "")) || 0;
+    let Expense = parseFloat(totalExpense.innerHTML.replace(/,/g, "")) || 0;
     let Budget = Income - Expense;
+     if (Income === 0 && Expense > 0) {
+     Budget = 0;}
+
     totalBudget.innerHTML = formatMoney(Budget);
 }
 calculateBudget();
@@ -61,26 +76,38 @@ calculateBudget();
 /**
  * Task 3: Delete Entry
  */
-function deleteEntry() {const listItem = this.closest("li");
-const list = listItem.parentElement;
-const valueString = listItem.querySelector(".text-green-600, .text-red-600").innerHTML.replace(/,/g, "");
-const value = parseFloat(valueString);
 
 
-list.removeChild(listItem);
+document.body.addEventListener("click", function(event) {
+  if (event.target.matches("#income-list .text-red-500, #expense-list .text-red-500")) {
+    deleteEntry.call(event.target);
+  }
+});
+
+function deleteEntry() {
+  const listItem = this.closest("li");
+  const list = listItem.parentElement;
+  const valueString = listItem.querySelector(".text-green-600, .text-red-600").innerHTML.replace(/,/g, "");
+  const value = parseFloat(valueString);
 
 
-if (list.id === "income-list") {
-  calculateIncome();
-} else if (list.id === "expense-list") {
-  calculateExpense();
-}
+  list.removeChild(listItem);
+
+  
+  if (list.id === "income-list") {
+    calculateIncome();
+  } else if (list.id === "expense-list") {
+    calculateExpense();
+  }
+
+  // Recalculate the budget after deleting an item
+  calculateBudget();
+  
 }
 const existingDeleteButtons = document.querySelectorAll("#income-list .text-red-500, #expense-list .text-red-500");
 existingDeleteButtons.forEach(button => {
-button.addEventListener("click", deleteEntry);
+  button.addEventListener("click", deleteEntry);
 });
-
 
 function addEntry() {
   const type = selectInput.value;
@@ -127,7 +154,7 @@ function addEntry() {
   // update total income value
   calculateIncome();
   calculateExpense();
-  
+  calculateBudget();
 }
 
 addExpenseButton.addEventListener("click", addEntry);
